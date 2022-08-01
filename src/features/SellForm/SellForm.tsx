0d862b67bb4844bf40928a640b0984087014572b
Sabahatt <109886872+Sabahatt@users.noreply.github.com>
2022-08-01 22:26:22 +0500

    styled for brandatron

--- src/features/SellForm/SellForm.tsx
+++ src/features/SellForm/SellForm.tsx
@@ -15,22 +15,30 @@ export const SellForm: React.FC<{}> = () => {
             </div>
             <div>
             <div className={styles.submitform}>
-                <h1>Input fields</h1>
+                <h1>Let's get you started with your first sale!</h1>
                 <label>Name: </label>
-                <input type="text" />
+                <input type="text" placeholder='Enter your full name' />
                 <br />
                 <label>Email: </label>
-                <input type="text" />
+                <input type="text" placeholder='Enter a valid email' />
                 <br />
-                <label>Shoe Description: </label>
-                <input type="text" />
-                <br />
+                
                 <label>Asking Price: </label>
-                <input type="text" />
+                <input type="text" placeholder='Enter your selling price'/>
                 <br />
+                <label>Condition:</label>
+                <select id="condition" name="condition">
+                    <option value="New">New</option>
+                    <option value="Slightly worn">Slightly worn</option>
+                    <option value="More than slightly worn">More than slightly worn</option>
+                    <option value="Quite worn">Quite worn</option>
+                </select>
+                <br/>
                 <label>Pictures: </label>
                 <input type="file" />
                 <br />
+                <label>Shoe Description: </label>
+                <input className={styles.description} type="text" placeholder='Please enter the brand of the shoe and any other details' />
                 <br />
                 <button onClick= {()=> {navigateTo('/sellForm/submit');}}>SELL</button>
             </div>
