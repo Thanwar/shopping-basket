3ea2dc6d57fdf3b72f6b487e177ae2a67f9de989
Thanwar <60439635+Thanwar@users.noreply.github.com>
2023-11-02 12:07:03 +0500

    Update NavBar.tsx

--- src/features/NavBar/NavBar.tsx
+++ src/features/NavBar/NavBar.tsx
@@ -14,7 +14,7 @@ export const NavBar: React.FC<{}> = () => {
         <div className={styles.container}>
             <h1 className={styles.logo}>
                 <Link to="./" data-testid="logo-navlink">
-                    Brandatron
+                    Brandatron 2
                 </Link>
             </h1>
             <label htmlFor="checkbox" className={styles.menuBtnWrapper}>
